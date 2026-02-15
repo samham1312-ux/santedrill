28024dcb952201b5544393955c5845f82d9eba32
samham1312-ux <samham1312@gmail.com>
2026-02-15 02:32:01 +0100

    Update route.js

--- app/sitemap.xml/route.js
+++ app/sitemap.xml/route.js
@@ -1,13 +1,26 @@
+import domaines from '../../content/domaines.json'
+
 export function GET() {
   const baseUrl = 'https://santedrill.vercel.app'
 
+  const urls = [
+    `${baseUrl}/`,
+    `${baseUrl}/domaines`,
+    ...domaines.map((domaine) => `${baseUrl}/quiz/${domaine.slug}`)
+  ]
+
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls
+  .map(
+    (url) => `
   <url>
-    <loc>${baseUrl}/</loc>
+    <loc>${url}</loc>
     <changefreq>weekly</changefreq>
-    <priority>1.0</priority>
-  </url>
+    <priority>0.8</priority>
+  </url>`
+  )
+  .join('')}
 </urlset>`
 
   return new Response(xml, {
