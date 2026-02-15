076c46488607f2f015541cd116c6cec83b91d9d8
samham1312-ux <samham1312@gmail.com>
2026-02-15 02:57:12 +0100

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,24 +1,23 @@
-import domainesData from '../content/domaines.json'
-
 export default function sitemap() {
   const baseUrl = 'https://santedrill.vercel.app'
 
-  // Accepte plusieurs formes possibles de JSON
-  const domaines = Array.isArray(domainesData)
-    ? domainesData
-    : (domainesData?.domaines || domainesData?.items || [])
-
-  const domaineUrls = domaines
-    .map((d) => d?.slug || d?.id || d?.code || d?.name)
-    .filter(Boolean)
-    .map((slug) => ({
-      url: `${baseUrl}/quiz/${slug}`,
-      lastModified: new Date(),
-    }))
+  const slugs = [
+    'anatomie',
+    'bio-fondamentale',
+    'droit-ethique',
+    'hygiene-infectiologie',
+    'pharmacologie',
+    'physiologie',
+    'psychologie',
+    'soins-surveillance',
+  ]
 
   return [
     { url: baseUrl, lastModified: new Date() },
     { url: `${baseUrl}/domaines`, lastModified: new Date() },
-    ...domaineUrls,
+    ...slugs.map((slug) => ({
+      url: `${baseUrl}/quiz/${slug}`,
+      lastModified: new Date(),
+    })),
   ]
 }
