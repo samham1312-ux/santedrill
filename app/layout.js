23cb7204dc58d68adb62f6ec8a438229b8ddbd40
samham1312-ux <samham1312@gmail.com>
2026-02-15 02:18:52 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -10,13 +10,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="_b3fm2AxOVPiw8XLLv0vbOUbycCHoUjiDyaXpBfLSIU"
+        />
+      </head>
       <body className="min-h-screen flex flex-col bg-gray-50">
         <Header />
-        <main className="flex-1">
-          {children}
-        </main>
+        <main className="flex-1">{children}</main>
         <Footer />
       </body>
     </html>
   )
 }
+
