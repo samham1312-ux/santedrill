311688c8b684dc923cb6d833abf767ebb18eae6f
samham1312-ux <samham1312@gmail.com>
2026-02-15 02:40:33 +0100

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -3,20 +3,12 @@ import domaines from '../content/domaines.json'
 export default function sitemap() {
   const baseUrl = 'https://santedrill.vercel.app'
 
-  const urls = [
-    {
-      url: baseUrl,
-      lastModified: new Date(),
-    },
-    {
-      url: `${baseUrl}/domaines`,
-      lastModified: new Date(),
-    },
-    ...domaines.map((domaine) => ({
-      url: `${baseUrl}/quiz/${domaine.slug}`,
+  return [
+    { url: baseUrl, lastModified: new Date() },
+    { url: `${baseUrl}/domaines`, lastModified: new Date() },
+    ...domaines.map((d) => ({
+      url: `${baseUrl}/quiz/${d.slug}`,
       lastModified: new Date(),
     })),
   ]
-
-  return urls
 }
