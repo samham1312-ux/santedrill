87074578f97691caab8bade17913a2493da821fd
samham1312-ux <samham1312@gmail.com>
2026-02-15 02:39:21 +0100

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,1 +1,22 @@
-// deleted
+import domaines from '../content/domaines.json'
+
+export default function sitemap() {
+  const baseUrl = 'https://santedrill.vercel.app'
+
+  const urls = [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/domaines`,
+      lastModified: new Date(),
+    },
+    ...domaines.map((domaine) => ({
+      url: `${baseUrl}/quiz/${domaine.slug}`,
+      lastModified: new Date(),
+    })),
+  ]
+
+  return urls
+}
